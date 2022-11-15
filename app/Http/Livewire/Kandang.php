996d362c3b2cb24cd1b26c6b8377dacfdb3737b1
Bahri <aribahri15@gmail.com>
2022-11-15 16:07:37 +0700

    add dummy

--- app/Http/Livewire/Kandang.php
+++ app/Http/Livewire/Kandang.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Cage;
 use Livewire\Component;
 
 class Kandang extends Component
 {
+    public $cage;
+
+    public function mount(){
+        $this->cage = Cage::get();
+    }
     public function render()
     {
         return view('livewire.kandang')->layout('layouts.app', ['title' => 'Admin - Kandang']);
--- resources/views/livewire/kandang.blade.php
+++ resources/views/livewire/kandang.blade.php
@@ -1,4 +1,14 @@
 <div>
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
     <x-co-tabel-kandang />
+    <livewire:kandang-table></livewire:kandang-table>
+    <table>
+        <tbody>
+        @foreach($cage as $item)
+        <td>
+            <tr></tr>
+        </td>
+        @endforeach
+        </tbody>
+    </table>
 </div>
